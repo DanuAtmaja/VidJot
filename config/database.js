ef1f80a6f3c3130b5779134dfa5c4434e92a93fc
Danu <danuatmaja@gmail.com>
2019-12-29 15:52:55 +0700

    solve the error

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://CHANGEME'}
+  module.exports = {mongoURI: 'mongodb+srv://drnfx25:pedrosa26@vidjot-prod-9eiig.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
