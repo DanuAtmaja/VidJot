a465f4aa89b15d134901ca21fa3e1ad500fb82f2
Danu <danuatmaja@gmail.com>
2019-12-29 12:02:00 +0700

    trying to solve an error

--- config/database.js
+++ config/database.js
@@ -1,19 +1,5 @@
-const e = require("express");
-
-if(process.envNODE_ENV === 'production') {
-  module.exports = { mongoURI = 'mongodb+srv://drnfx25:<password>@vidjot-prod-9eiig.mongodb.net/test?retryWrites=true&w=majority'}
-}else {
-  module.exports = {mongoURI = 'mongodb://localhost/vidjot-dev'}
+if(process.env.NODE_ENV === 'production'){
+  module.exports = {mongoURI: 'mongodb://CHANGEME'}
+} else {
+  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
-
-
-
-// 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://drnfx25:<password>@vidjot-prod-9eiig.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
